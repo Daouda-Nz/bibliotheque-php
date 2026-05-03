e609881f65544605155d056ce71fee0a7cfe02b7
Daouda Diakhaté <diakhatedaouda07@icloud.com>
2026-05-03 19:21:18 +0000

    formulaire auteur + affichage

--- public/auteurs.php
+++ public/auteurs.php
@@ -1,15 +1,44 @@
 <?php
-
 require_once "../classes/Auteur.php";
 
 $auteur = new Auteur();
 
-// Ajouter un auteur (test)
-$auteur->ajouter("Victor", "Hugo", "Française");
+// Ajouter si formulaire envoyé
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom = $_POST["nom"];
+    $prenom = $_POST["prenom"];
+    $nationalite = $_POST["nationalite"];
 
-// Afficher les auteurs
+    $auteur->ajouter($nom, $prenom, $nationalite);
+}
+
+// Récupérer liste
 $liste = $auteur->afficher();
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gestion des auteurs</title>
+</head>
+<body>
+
+<h2>Ajouter un auteur</h2>
+
+<form method="POST">
+    Nom : <input type="text" name="nom"><br><br>
+    Prénom : <input type="text" name="prenom"><br><br>
+    Nationalité : <input type="text" name="nationalite"><br><br>
+    <button type="submit">Ajouter</button>
+</form>
+
+<h2>Liste des auteurs</h2>
+
+<?php
 foreach ($liste as $a) {
     echo $a["id"] . " - " . $a["nom"] . " " . $a["prenom"] . "<br>";
 }
+?>
+
+</body>
+</html>
